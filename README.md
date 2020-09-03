9750563465ff44fc1967222d0bc435b7269d4fa0
Thomas Mahler <thma32@web.de>
2020-09-03 21:25:02 +0200

    onwards

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TypelevelReflection
 Using haskell typelevel programming to implement reflective features. This can be used to eg. implement persistence or serialization.
+
+## about
+
+I'm following chapter 13 of "Thinking with types" to implement a some building blocks of a most simple persistence framework 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE TypeOperators #-} -- :*: and :+:
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib
     ( someFunc
     ) where
 
+import GHC.Generics
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+class GEq a where
+  geq :: a x -> a x -> Bool
+
+instance GEq U1 where
+  geq U1 U1 = True
+
+instance GEq V1 where
+  geq _ _ = True
+
+instance Eq a => GEq (K1 _l a) where
+  geq (K1 a) (K1 b) = a == b
+
+instance (GEq a, GEq b) => GEq (a :+: b) where
+  geq (L1 a1) (L1 a2) = geq a1 a2
+  geq (R1 b1) (R1 b2) = geq b1 b2
+  geq _ _             = False
+
+instance (GEq a, GEq b) => GEq (a :*: b) where
+  geq (a1 :*: b1) (a2 :*: b2) = geq a1 a2 && geq b1 b2
+
+data Foo a b c =
+    F0
+  | F1 a
+  | F2 b c
+  deriving (Generic)
